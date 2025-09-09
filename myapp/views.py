7784aeb51c3f7489e9e3c28b19a69636eb29386f
Eddy <eddyfrank317@gmail.com>
2025-09-09 13:35:20 +0300

    Refactor templates and views for consistency and readability

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 
-
 def index(request):
     return render(request, "index.html") 
-
-
 
 def about(request):
     return render(request, "about.html")
@@ -14,6 +11,4 @@ def contact(request):
 
 
 # Create your views here.
-
-
 
